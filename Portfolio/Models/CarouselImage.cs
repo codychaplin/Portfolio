b7d80671d09ecba80c64d2e27fcffbee2cb04a6b
Cody Chaplin <codybchaplin@gmail.com>
2023-03-12 19:20:39 -0400

    Added Projects section.

---
+++ Portfolio/Models/CarouselImage.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Models;
+
+public class CarouselImage
+{
+    public string Src { get; private set; }
+    public string Alt { get; private set; }
+
+    public CarouselImage(string src, string alt)
+    {
+        Src = src;
+        Alt = alt;
+    }
+}
